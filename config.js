e540398b7144293a5770b9c8edade53ac51cfb1a
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-05-24 13:37:42 +0530

    Update Token to Janas bot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAPzVidMnn0BAG0T2DdOZAgAyRm67cBgQa897bdsHY5Gc2OZB8MfA1Hqc90yRc0ZBlAKY5s1VYBqa99ORMWMTi6L2cK8U88x3EOo8l2mKBQXiEOpwuN3yFHs7bfZAWy7cSEfTUFvQNBif9ti02mdLJHlZCZBkoeX9G7XDQWDv8bAZDZD',
-    FB_VERIFY_TOKEN: 'my_voice_and_my_password-ud-co-bot1',
-    API_AI_CLIENT_ACCESS_TOKEN: '5534e804393d44a7b5f3639e7a2f1d7a',
+    FB_VERIFY_TOKEN: 'my_voice_is_my_password123456789',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a0608bd94bc04fe8bacab682d44fceea',
     FB_APP_SECRET: '24b371ee1cd2768e770994a865053bd2',
     SERVER_URL: "http://salty-hollows-44528.herokuapp.com/",
     SENGRID_API_KEY: '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',
